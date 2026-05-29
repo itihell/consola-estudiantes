3e617f4e80d84dbed613222fbb1a4f765210e887
Itihell Mejia <itihell.mejia@gmail.com>
2026-05-29 16:55:50 -0600

    Guardar datos desdde el main

--- app/models/carrera.js
+++ app/models/carrera.js
@@ -1,7 +1,7 @@
 import Model from "./model.js";
 
 export default class Carrera extends Model {
-  #table = "carreras";
+  table = "carreras";
   constructor(nombre) {
     super();
     this.id = Date.now();
--- app/models/persona.js
+++ app/models/persona.js
@@ -1,7 +1,8 @@
-import Model from "./model";
+import Model from "./model.js";
 export default class Persona extends Model {
-  #table = "personas";
+  table = "personas";
   constructor(nombre, edad, sexo, carrera) {
+    super();
     this.id = Date.now();
     this.nombre = nombre;
     this.edad = edad;
