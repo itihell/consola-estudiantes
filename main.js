e7494acc646bf6cf65d26d95501909f6d30e51c9
Itihell Mejia <itihell.mejia@gmail.com>
2026-05-29 16:04:31 -0600

    Guardando varios registros

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import Carrera from "./app/models/carrera.js";
 async function main() {
-  const carrera = new Carrera("Ingeniería en Software");
+  const carrera = new Carrera("Administracion de Empresas");
   const payload = await carrera.save(carrera);
   console.log("Carrera guardada:", payload);
 }
